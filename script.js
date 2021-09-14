671952fdd110d3ba351088b2db3b3a3b1b7b0625
Даниил Никитин <faverus061999@gmail.com>
2021-09-14 20:25:34 +0300

    task 4

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
-let first = Number(prompt("Введите первое значение"));
-let second = Number(prompt("Введите второе значение"));
+function concatStrings(word, separator) {
+  return (next) => {
+    if (typeof next !== "string") {
+      return word;
+    }
 
-if (!first || !second) {
-  alert("Неверный ввод");
-} else {
-  let newNumber = first.toString(second);
-  alert(newNumber);
+    if (typeof separator !== "string") {
+      const newString = word + next;
+      return concatStrings(newString);
+    }
+
+    const newString = word + separator + next;
+    return concatStrings(newString, separator);
+  };
+}
+
+class Calculator {
+  constructor(x, y) {
+    if (
+      typeof x !== "number" ||
+      typeof y !== "number" ||
+      !isFinite(x) ||
+      !isFinite(y) ||
+      isNaN(x) ||
+      isNaN(y)
+    ) {
+      throw new Error("Ошибка");
+    }
+    this.x = x;
+    this.y = y;
+  }
+
+  setX = (num) => {
+    if (typeof num !== "number" || !isFinite(num) || isNaN(num)) {
+      throw new Error("Ошибка");
+    }
+    this.x = num;
+  };
+
+  setY = (num) => {
+    if (typeof num !== "number" || !isFinite(num) || isNaN(num)) {
+      throw new Error("Ошибка");
+    }
+    this.y = num;
+  };
+
+  logSum = () => {
+    console.log(this.x + this.y);
+  };
+
+  logMul = () => {
+    console.log(this.x * this.y);
+  };
+
+  logSub = () => {
+    console.log(this.x - this.y);
+  };
+
+  logDiv = () => {
+    if (this.y === 0) {
+      throw new Error("Ошибка!");
+    }
+    console.log(this.x / this.y);
+  };
 }
